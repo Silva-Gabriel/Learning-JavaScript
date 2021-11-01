04342e8cdae2f913f52ef77a64b16d5a35873579
Silva-Gabriel <pottergryff8@gmail.com>
2021-10-31 21:04:47 -0300

    Finaliza calculadora simples em JS

--- Iniciando/JS/script.js
+++ Iniciando/JS/script.js
@@ -4,55 +4,76 @@ var button = document.querySelector('.btn');
 var simboloOperacao = document.getElementById('operacao');
 var tn1 = document.getElementById('txt1');
 var tn2 = document.querySelector('input#txt2');
-var funcaoAtiva = false;
+var operacao;
+var buttonRld = document.querySelector('.btnReload');
+buttonRld.addEventListener('click',main);
+var n1;
+var n2;
+var resultado;
+
 
 //Criando menu
-var operacao = prompt('Escolha uma operação:\n[1] Soma\n[2] Subtração\n[3] Divisão\n[4] Resto da divisão\n[5] Multiplicação\n[6] Potenciação\n');
+function calculadora()
+{
+    operacao = prompt('Escolha uma operação:\n[1] Soma\n[2] Subtração\n[3] Divisão\n[4] Resto da divisão\n[5] Multiplicação\n[6] Potenciação\n');
+}
 
-switch(operacao){
-    case '1':
-        modifyTitle.innerText = 'Somar';
-        simboloOperacao.innerText = '+';
-        button.addEventListener("click",somar);
-        button.addEventListener("click",mostrarResultado);
-        break;
-    case '2':
-        modifyTitle.innerText = 'Subtrair';
-        simboloOperacao.innerText = '-';
-        button.addEventListener('click',subtrair);
-        button.addEventListener("click",mostrarResultado);
-        break;
-    case '3':
-        modifyTitle.innerText = 'Dividir';
-        simboloOperacao.innerText = '÷';
-        button.addEventListener('click',divisao);
-        break;
-    case '4':
-        modifyTitle.innerText = 'Resto da divisão';
-        simboloOperacao.innerText = '%';
-        button.addEventListener('click',divisaoResto);
-        break;
-    case '5':
-        modifyTitle.innerText = 'multiplicação';
-        simboloOperacao.innerText = 'x';
-        button.addEventListener('click',multiplicacao);
-        break;
-    case '6':
-        modifyTitle.innerText = 'Potencia';
-        simboloOperacao.innerText = '^';
-        button.addEventListener('click',potencia);
-        break;
-    default:
-        validarOperacao();
+function main()
+{
+    calculadora();
+    if(validarOperacao() == true)
+        iniciarOperacao();
+}
+
+function iniciarOperacao()
+{
+    switch(operacao){
+        case '1':
+            modifyTitle.innerText = 'Somar';
+            simboloOperacao.innerText = '+';
+            button.addEventListener("click",somar);
+            button.addEventListener("click",mostrarResultado);
+            break;
+        case '2':
+            modifyTitle.innerText = 'Subtrair';
+            simboloOperacao.innerText = '-';
+            button.addEventListener('click',subtrair);
+            button.addEventListener("click",mostrarResultado);
+            break;
+        case '3':
+            modifyTitle.innerText = 'Dividir';
+            simboloOperacao.innerText = '÷';
+            button.addEventListener('click',divisao);
+            button.addEventListener("click",mostrarResultado);
+            break;
+        case '4':
+            modifyTitle.innerText = 'Resto da divisão';
+            simboloOperacao.innerText = '%';
+            button.addEventListener('click',divisaoResto);
+            button.addEventListener("click",mostrarResultado);
+            break;
+        case '5':
+            modifyTitle.innerText = 'multiplicação';
+            simboloOperacao.innerText = 'x';
+            button.addEventListener('click',multiplicacao);
+            button.addEventListener("click",mostrarResultado);
+            break;
+        case '6':
+            modifyTitle.innerText = 'Potencia';
+            simboloOperacao.innerText = '^';
+            button.addEventListener('click',potencia);
+            button.addEventListener("click",mostrarResultado);
+            break;
+    }
 }
 
 function validarOperacao()
 {
-    let opcaoUsuario = prompt('Operação inválida!\nDeseja fazer outra operação?\n\n[1] Sim\n[2] Não');
-    if(opcaoUsuario == 1)
-        location.reload();
+    if(Number(operacao) >= 1 && Number(operacao) <= 6)
+        return true;
     else{
-        window.close();
+        alert('Escolha uma opção válida!');
+        main();
     }
 }
 
@@ -67,51 +88,52 @@ function mostrarResultado()
     total.style.marginTop = '10px';
 }
 
+function getValorInput(){
+    n1 = Number(tn1.value);
+    n2 = Number(tn2.value);
+}
+
 function somar(){
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    var resultado = n1 + n2;
+    getValorInput();
+    resultado = n1 + n2;
     total.innerHTML = `${resultado}`;
     return resultado;
 }
 
 function subtrair(){
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    var resultado = n1 - n2;
+    getValorInput();
+    resultado = n1 - n2;
     total.innerHTML = `${resultado}`;
     return resultado;
 }
 
 function divisao(){
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    var resultado = n1 / n2;
+    getValorInput();
+    resultado = n1 / n2;
     total.innerHTML = `${resultado}`;
     return resultado;
 }
 
 function divisaoResto(){
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    var resultado = n1 % 2;
+    getValorInput();
+    resultado = n1 % n2;
     total.innerHTML = `${resultado}`;
     return resultado;
 }
 
 function multiplicacao(){
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    var resultado = n1 * n2;
+    getValorInput();
+    resultado = n1 * n2;
     total.innerHTML = `${resultado}`;
     return resultado;
 }
 
 function potencia(){
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    var valorN1 = n1;
+    getValorInput();
+    valorN1 = n1;
     for(let i = 0; i < n2-1;i++)
         n1 *= valorN1;
     total.innerHTML = `${n1}`;
 }
+
+main();
